151ef1f374a2dea4099c975b01e6c1713f13d62a
Alvaro Mayo <alvaromayov@gmail.com>
2021-01-21 19:06:32 +0000

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,46 +1,14 @@
+import React from 'react';
 import './App.css';
-import React, { Component } from 'react';
 
-import { CardList } from './components/card-list/card-list';
-import { SearchBox } from './components/search-box/search-box';
+import HomePage from './pages/homepage/HomePage.component';
 
-class App extends Component {
-
-  constructor() {
-    super();
-
-    this.state = {
-      persons: [],
-      searchField: ""
-    }
-  }
-
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(users => this.setState({ persons: users }))
-  }
-
-  handleChange = (e) => {
-    this.setState({ searchField: e.target.value })
-  }
-
-  render() {
-    const { persons, searchField } = this.state;
-    const filteredpersons = persons.filter(person =>
-      person.name.toLowerCase().includes(searchField.toLocaleLowerCase())
-    )
-    return (
-      <div className="App">
-        <SearchBox
-            placeholder="Search person"
-            handleChange={this.handleChange}
-          />
-          <h1>Employee Rolodex</h1>
-        <CardList persons={filteredpersons} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
