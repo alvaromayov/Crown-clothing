16e5a37b4f990cd99ab94631ce3d2404b7e68469
Alvaro Mayo <alvaromayov@gmail.com>
2021-01-25 21:39:02 +0000

    create sign in page and component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/HomePage.component';
 import Shop from './pages/shop/shop.component';
-import Header from './components/header/header.component';
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={Shop} />
+        <Route path="/signIn" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,7 +11,8 @@ const Header = () => (
         </Link>
         <div className="options">
         <Link className="option" to="/shop">Shop</Link>
-
+        <Link className="option" to="/contact">Contact</Link>
+        <Link className="option" to="/signin">Sign in</Link>
         </div>
     </div>
 )
